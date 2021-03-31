337cc387b3a2ed3a3872d3c834f9f9f18dd9f53d
quinaiton <quinaiton@hotmail.com>
2021-03-31 14:22:23 -0700

    Finishes Gallery Component for Large Screens

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
-import Layout from '../components/Layout';
+import Nav from '../components/Nav';
 import '../styles/globals.scss';
 import '../styles/nav.scss';
 import '../styles/index.scss';
 import '../styles/services.scss';
+import '../styles/gallery.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
+      <Nav />
       <Component {...pageProps} />
-    </Layout>
+    </>
   );
 }
 
--- pages/gallery.js
+++ pages/gallery.js
@@ -8,312 +8,312 @@ const gallery = () => {
       <div class='gallery-content'>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img0-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img1-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img2-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img3-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img4-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img5-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img6-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img7-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img8-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img9-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img10-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img11-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img12-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img13-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img14-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img15-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img16-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img17-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img18-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img19-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img20-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img21-min.jpg'
             alt='pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img22-min.jpg'
             alt='pressure pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img23-min.jpg'
             alt='pressure pipe welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img24-min.jpg'
             alt='excavator bucket repair'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img25-min.jpg'
             alt='logging machine welding repair'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img26-min.jpg'
             alt='industrial mobile welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img27-min.jpg'
             alt='logging machine welding'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img28-min.jpg'
             alt='maching welding repair'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img29-min.jpg'
             alt='machine welding repair'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img30-min.jpg'
             alt='machine logging repair'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img31-min.jpg'
             alt='industral mobile welding repair'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img32-min.jpg'
             alt='industrial mobile welding trucks'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img33-min.jpg'
             alt='industrial mobile welding repair job'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img34-min.jpg'
             alt='pipeline welding repair'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img35-min.jpg'
             alt='custom gate welding repair'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img36-min.jpg'
             alt='structrual welding services'
           />
         </div>
         <div class='gallery-item'>
           <Image
-            height={200}
-            width={200}
+            height={500}
+            width={500}
             src='/img37-min.jpg'
             alt='custom welding roll cage'
           />
